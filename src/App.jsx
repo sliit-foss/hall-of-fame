df15de9f1d3959e8daaf72085ab4010a525c997b
Thulina Wickramasinghe <thulinawickramasinghe@gmail.com>
2024-08-04 12:21:05 +0530

    Feat: setup githooks, prettier,eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-    <h1 className="text-green-600">Hall of Fame</h1>
+      <h1 className="text-green-600">Hall of Fame</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
